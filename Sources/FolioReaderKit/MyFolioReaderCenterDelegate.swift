d52fda62c655af17e8a905bd10492cac457db54c
Peter <peterli@163.com>
2021-04-06 22:49:46 +0800

    WIP: apply MyFolioReaderCenterDelegate

---
+++ Sources/FolioReaderKit/MyFolioReaderCenterDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  
+//
+//  Created by 京太郎 on 2021/4/6.
+//
+
+import Foundation
+
+class MyFolioReaderCenterDelegate: FolioReaderCenterDelegate {
+    
+    @objc func htmlContentForPage(_ page: FolioReaderPage, htmlContent: String) -> String {
+        
+        // print(htmlContent)
+        let regex = try! NSRegularExpression(pattern: "background=\"[^\"]+\"", options: .caseInsensitive)
+        
+        
+        let modified = regex.stringByReplacingMatches(in: htmlContent, options: [], range: NSMakeRange(0, htmlContent.count), withTemplate: "").replacingOccurrences(of: "<body ", with: "<body style=\"text-align: justify !important; display: block !important; \" ")
+        print(modified)
+        return modified
+    }
+}
