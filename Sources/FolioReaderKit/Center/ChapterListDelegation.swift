b530ffb8b339b44fda858607c276f54a7e5bdd69
Drearycold <roswen9@gmail.com>
2022-04-09 13:01:32 +0800

    chapter scroll toolbar offset

--- Sources/FolioReaderKit/Center/ChapterListDelegation.swift
+++ Sources/FolioReaderKit/Center/ChapterListDelegation.swift
@@ -35,7 +35,7 @@ extension FolioReaderCenter: FolioReaderChapterListDelegate {
         // Move to #fragment
         if let reference = tempReference {
             if let fragmentID = reference.fragmentID, let currentPage = currentPage , fragmentID != "" {
-                currentPage.handleAnchor(reference.fragmentID!, offsetInWindow: 0, avoidBeginningAnchors: true, animated: true)
+                currentPage.handleAnchor(reference.fragmentID!, offsetInWindow: self.navigationController?.toolbar.frame.height ?? 0, avoidBeginningAnchors: true, animated: true)
             }
             tempReference = nil
         }
