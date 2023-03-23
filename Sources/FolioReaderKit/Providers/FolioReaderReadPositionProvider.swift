a1f3f2cdf214a7273f7018c5b3d6aa69c51042cb
Doug Russell <doug@getitdownonpaper.com>
2023-03-23 13:12:13 -0400

    FolioReaderReadPositionProvider doesn't need to be @objc

--- Sources/FolioReaderKit/Providers/FolioReaderReadPositionProvider.swift
+++ Sources/FolioReaderKit/Providers/FolioReaderReadPositionProvider.swift
@@ -7,41 +7,68 @@
 
 import Foundation
 
-@objc public protocol FolioReaderReadPositionProvider: AnyObject {
-
+public protocol FolioReaderReadPositionProvider: AnyObject {
     /**
         get latest one, considing precedence
      */
-    @objc func folioReaderReadPosition(_ folioReader: FolioReader, bookId: String) -> FolioReaderReadPosition?
+    func folioReaderReadPosition(
+        _ folioReader: FolioReader,
+        bookId: String
+    ) -> FolioReaderReadPosition?
+
+    func folioReaderReadPosition(
+        _ folioReader: FolioReader,
+        bookId: String,
+        by rootPageNumber: Int
+    ) -> FolioReaderReadPosition?
+
+    func folioReaderReadPosition(
+        _ folioReader: FolioReader,
+        bookId: String,
+        set readPosition: FolioReaderReadPosition,
+        completion: Completion?
+    )
+
+    func folioReaderReadPosition(
+        _ folioReader: FolioReader,
+        bookId: String,
+        remove readPosition: FolioReaderReadPosition
+    )
+
+    func folioReaderReadPosition(
+        _ folioReader: FolioReader,
+        bookId: String,
+        getById deviceId: String
+    ) -> [FolioReaderReadPosition]
     
-    @objc func folioReaderReadPosition(_ folioReader: FolioReader, bookId: String, by rootPageNumber: Int) -> FolioReaderReadPosition?
-    
-    @objc func folioReaderReadPosition(_ folioReader: FolioReader, bookId: String, set readPosition: FolioReaderReadPosition, completion: Completion?)
-    
-    @objc func folioReaderReadPosition(_ folioReader: FolioReader, bookId: String, remove readPosition: FolioReaderReadPosition)
-    
-    @objc func folioReaderReadPosition(_ folioReader: FolioReader, bookId: String, getById deviceId: String) -> [FolioReaderReadPosition]
-    
-    @objc func folioReaderReadPosition(_ folioReader: FolioReader, allByBookId bookId: String) -> [FolioReaderReadPosition]
-    
-    @objc func folioReaderReadPosition(_ folioReader: FolioReader) -> [FolioReaderReadPosition]
-    
-    @objc func folioReaderPositionHistory(_ folioReader: FolioReader, bookId: String) -> [FolioReaderReadPositionHistory]
-    
-//    @objc optional func folioReaderPositionHistory(_ folioReader: FolioReader, bookId: String, start readPosition: FolioReaderReadPosition)
-    
-//    @objc optional func folioReaderPositionHistory(_ folioReader: FolioReader, bookId: String, finish readPosition: FolioReaderReadPosition)
-    
-//    @objc optional func folioReaderPositionHistory(_ folioReader: FolioReader, bookId: String, remove readPosition: FolioReaderReadPositionHistory)
+    func folioReaderReadPosition(
+        _ folioReader: FolioReader,
+        allByBookId bookId: String
+    ) -> [FolioReaderReadPosition]
+
+    func folioReaderReadPosition(_ folioReader: FolioReader) -> [FolioReaderReadPosition]
+
+    func folioReaderPositionHistory(
+        _ folioReader: FolioReader,
+        bookId: String
+    ) -> [FolioReaderReadPositionHistory]
+
+    //func folioReaderPositionHistory(_ folioReader: FolioReader, bookId: String, start readPosition: FolioReaderReadPosition)
+
+    //func folioReaderPositionHistory(_ folioReader: FolioReader, bookId: String, finish readPosition: FolioReaderReadPosition)
+
+    //func folioReaderPositionHistory(_ folioReader: FolioReader, bookId: String, remove readPosition: FolioReaderReadPositionHistory)
 }
 
 public class FolioReaderNaiveReadPositionProvider: FolioReaderReadPositionProvider {
-    
     var positions : [String: [String: FolioReaderReadPosition]] = [:]
     var history: [FolioReaderReadPositionHistory] = []
-    
-    public func folioReaderReadPosition(_ folioReader: FolioReader, bookId: String) -> FolioReaderReadPosition? {
-        return positions.flatMap { $0.value }.compactMap { $0.value }.max {
+
+    public func folioReaderReadPosition(
+        _ folioReader: FolioReader,
+        bookId: String
+    ) -> FolioReaderReadPosition? {
+        positions.flatMap { $0.value }.compactMap { $0.value }.max {
             if $0.takePrecedence == $1.takePrecedence {
                 return $0.epoch < $1.epoch
             }
@@ -49,49 +76,70 @@ public class FolioReaderNaiveReadPositionProvider: FolioReaderReadPositionProvid
         }
     }
     
-    public func folioReaderReadPosition(_ folioReader: FolioReader, bookId: String, by rootTocPageNumner: Int) -> FolioReaderReadPosition? {
-        return self.positions[bookId]?.filter {
+    public func folioReaderReadPosition(
+        _ folioReader: FolioReader,
+        bookId: String,
+        by rootTocPageNumner: Int
+    ) -> FolioReaderReadPosition? {
+        positions[bookId]?.filter {
             $0.value.structuralStyle == folioReader.structuralStyle
             && $0.value.positionTrackingStyle == folioReader.structuralTrackingTocLevel
             && $0.value.structuralRootPageNumber == rootTocPageNumner
         }.first?.value
     }
     
-    public func folioReaderReadPosition(_ folioReader: FolioReader, bookId: String, set lastReadPosition: FolioReaderReadPosition, completion: Completion?) {
-        if self.positions[bookId] == nil {
-            self.positions[bookId] = [:]
+    public func folioReaderReadPosition(
+        _ folioReader: FolioReader,
+        bookId: String,
+        set lastReadPosition: FolioReaderReadPosition,
+        completion: Completion?
+    ) {
+        if positions[bookId] == nil {
+            positions[bookId] = [:]
         }
-        self.positions[bookId]?[lastReadPosition.deviceId] = lastReadPosition
+        positions[bookId]?[lastReadPosition.deviceId] = lastReadPosition
     }
     
-    public func folioReaderReadPosition(_ folioReader: FolioReader, bookId: String, remove readPosition: FolioReaderReadPosition) {
-        self.positions[bookId]?.removeValue(forKey: readPosition.deviceId)
+    public func folioReaderReadPosition(
+        _ folioReader: FolioReader,
+        bookId: String,
+        remove readPosition: FolioReaderReadPosition
+    ) {
+        positions[bookId]?.removeValue(forKey: readPosition.deviceId)
     }
     
-    public func folioReaderReadPosition(_ folioReader: FolioReader, bookId: String, getById deviceId: String) -> [FolioReaderReadPosition] {
-        if let position = self.positions[bookId]?[deviceId] {
+    public func folioReaderReadPosition(
+        _ folioReader: FolioReader,
+        bookId: String,
+        getById deviceId: String
+    ) -> [FolioReaderReadPosition] {
+        if let position = positions[bookId]?[deviceId] {
             return [position]
         } else {
             return []
         }
     }
     
-    public func folioReaderReadPosition(_ folioReader: FolioReader, allByBookId bookId: String) -> [FolioReaderReadPosition] {
-        return self.positions[bookId]?.values.map { $0 } ?? []
+    public func folioReaderReadPosition(
+        _ folioReader: FolioReader,
+        allByBookId bookId: String
+    ) -> [FolioReaderReadPosition] {
+        positions[bookId]?.values.map { $0 } ?? []
     }
     
     public func folioReaderReadPosition(_ folioReader: FolioReader) -> [FolioReaderReadPosition] {
-        return self.positions.reduce(into: []) { partialResult, bookPositions in
+        positions.reduce(into: []) { partialResult, bookPositions in
             partialResult.append(contentsOf: bookPositions.value.values)
         }
     }
     
-    public init() {
-        
-    }
+    public init() {}
     
-    public func folioReaderPositionHistory(_ folioReader: FolioReader, bookId: String) -> [FolioReaderReadPositionHistory] {
-        return history
+    public func folioReaderPositionHistory(
+        _ folioReader: FolioReader,
+        bookId: String
+    ) -> [FolioReaderReadPositionHistory] {
+        history
     }
     
 }
